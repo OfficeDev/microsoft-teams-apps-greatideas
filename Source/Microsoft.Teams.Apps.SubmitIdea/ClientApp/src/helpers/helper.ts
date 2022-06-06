8fb22b96e7ecabe2237484c783b655044d287435
SHASHIDHAR_SUNAGAR <ssunagar@withum.digital>
2022-06-06 22:05:00 +0530

    Updated helper ts

--- Source/Microsoft.Teams.Apps.SubmitIdea/ClientApp/src/helpers/helper.ts
+++ Source/Microsoft.Teams.Apps.SubmitIdea/ClientApp/src/helpers/helper.ts
@@ -74,10 +74,10 @@ export const generateColor = () => {
  */
 export const getInitials = (userPostName: string) => {
   let fullName = userPostName;
-  let names = fullName.split(" "),
-    initials = names[0]?.substring(0, 1).toUpperCase();
+  let names = fullName?.split(" "),
+    initials = names &&names.length > 1 ?names[0]?.substring(0, 1).toUpperCase():"";
 
-  if (names.length > 1) {
+  if (names && names.length > 1) {
     initials += names[names.length - 1].substring(0, 1).toUpperCase();
   }
   return initials;
